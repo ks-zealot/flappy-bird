dff0f4e18fcfaed598e92e21f632dd99bb86c923
zealot <z10ba89@gmail.com>
2024-07-11 14:14:27 +0700

    add name

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React , { useState , useEffect} from 'react'
 
 export default function Menu({ score, onPlay, onReplay, onReverse }) {
+	const [username, setUsername] = useState('')
+	useEffect(() => {
+			/*global Telegram */
+			if (Telegram.WebApp.initDataUnsafe.user) {
+				const wb = Telegram.WebApp.initDataUnsafe.user
+				setUsername(wb.first_name)
+			} else {
+				setUsername('Test')
+			}
+		}
+	)
 	return (
 		<div className="menu c-wrap">
 			<ul className="c-inner">
-				<li>score: {score}</li>
+				<li>{username}, your score is: {score}</li>
 				<li>
 					<div className="btn" onMouseDown={onPlay} onTouchStart={onPlay}>play</div>
 				</li>
